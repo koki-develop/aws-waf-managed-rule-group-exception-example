09f0fed0ef186871a04c5096a23587642a59c767
koki-develop <kou.pg.0131@gmail.com>
2023-07-08 22:42:55 +0900

    Add Cloudfront and S3 resources in Terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "main" {
+  bucket_prefix = "example-"
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.s3_main_policy.json
+}
+
+data "aws_iam_policy_document" "s3_main_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.main.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.main.arn]
+    }
+  }
+}
