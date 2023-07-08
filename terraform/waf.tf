19bae2966b84e5acd9a5768224a095d1efadff95
koki-develop <kou.pg.0131@gmail.com>
2023-07-08 23:12:41 +0900

    Add visibility config and example rule to WAF config

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -31,6 +31,53 @@ resource "aws_wafv2_web_acl" "main" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
+      metric_name                = "example-managed-rule-metric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "ExampleRule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          label_match_statement {
+            key   = "awswaf:managed:aws:core-rule-set:NoUserAgent_Header"
+            scope = "LABEL"
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              byte_match_statement {
+                # /hello.txt 以外のパスに対して NoUserAgent_HEADER ルールを適用してブロックする
+                search_string         = "/hello.txt"
+                positional_constraint = "EXACTLY"
+
+                field_to_match {
+                  uri_path {}
+                }
+
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
       metric_name                = "example-rule-metric"
       sampled_requests_enabled   = true
     }
